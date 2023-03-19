136e0d83d0d7c701a436f70529dc7d86aa51e035
Maxime <sowlution.pro@gmail.com>
2023-03-19 10:27:45 +0100

    testnet sbt display OK

--- src/components/pages/VerifyProof.js
+++ src/components/pages/VerifyProof.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './styles/verify.css';
 
 function VerifyProof() {
   const [account, setAccount] = useState('');
@@ -11,7 +12,7 @@ function VerifyProof() {
   const handleFormSubmit = event => {
     event.preventDefault();
 
-    const ws = new WebSocket('wss://s.altnet.rippletest.net:51233');
+    const ws = new WebSocket('wss://s.altnet.rippletest.net:51233'); // Testnet
 
     ws.addEventListener('open', event => {
       const requestMessage = {
@@ -26,7 +27,7 @@ function VerifyProof() {
     ws.addEventListener('message', event => {
       const responseMessage = JSON.parse(event.data);
       if (responseMessage.type === 'response' && responseMessage.id === 1) {
-        setNFTs(responseMessage.result);
+        setNFTs(responseMessage.result.account_nfts);
       }
     });
 
@@ -41,14 +42,44 @@ function VerifyProof() {
       <p>💡<i>Only available on XRPL Mainnet and Testnet</i></p>
       <form onSubmit={handleFormSubmit}>
         <label>
-          Account:
+          <b>Account:</b>
           <input type="text" value={account} onChange={handleAccountChange} />
         </label>
-        <button type="submit" className='swap'>Check NFTs</button>
+        <button type="submit" className='swap'>Check solvency proof SBT</button>
       </form>
-      {nfts && <pre>{JSON.stringify(nfts, null, 2)}</pre>}
+      {nfts && (
+        <div className="card-container">
+          {nfts.map((nft, index) => (
+            <div key={index} className="card">
+              <div className="card-header">
+                <h2>SBT {index + 1}</h2>
+                <div className="status">{nft.Flags === 8 ? 'Valid' : 'Invalid'}</div>
+              </div>
+              <div className="card-content">
+                <p>
+                  <strong>Account:</strong> {nft.Issuer}
+                </p>
+                <p>
+                  <strong>NFTokenID:</strong> {nft.NFTokenID}
+                </p>
+                <p>
+                  <strong>URI:</strong> {hexToString(nft.URI)}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
+}
+
+function hexToString(hex) {
+  let string = '';
+  for (let i = 0; i < hex.length; i += 2) {
+    string += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  }
+  return string;
 }
 
 export default VerifyProof;
